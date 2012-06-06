d985d8e65b34ea2e40a05263d366c4adef9c4d0f
Eric D. Schabell <eric@schabell.org>
2012-06-06 15:18:28 +0200

    Added custom db filling.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+-- You can use this file to load seed data into the database using SQL statements
+insert into MemberHTML5 (id, name, email, phone_number) values (0, 'Eric D. Schabell', 'erics@mail.com', '1234567890')
